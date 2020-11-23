16b014fb7eefac4bae202c5c7539258528c45899
Gerson <44945972+ItsGers8@users.noreply.github.com>
2020-11-23 14:28:35 +0100

    Backend setup

---
+++ src/test/java/com/v2b2/Billy/BillyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v2b2.Billy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
